2ee5c6408c6113be68f4ad5833a13a794b87c88d
Zhenhai Zhang <94429047+zzhan039@users.noreply.github.com>
2022-03-14 14:37:49 -0700

    Add files via upload

---
+++ Untitled.m
@@ -0,0 +1,9 @@
+clear;
+load('hcptmp.mat');
+tmp = hcp_tmp;
+dir = 'H:\paperlalala\hcp1\';
+
+dir2 = '\rfMRI_REST1_LR.nii.gz';
+
+dir3 = '\rfMRI_REST1_LR.nii';
+ m = [];acep = [];mip=[];dis =[];FA_v = []; features =[];dimp=[];
